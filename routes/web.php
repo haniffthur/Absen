df030aba2e1df20bbdb53bcb8c2f67993bd9495e
haniffthur <haniffathur02@gmail.com>
2024-08-22 13:03:24 +0700

    absen

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get ('/dashboard',[DashboardController::class,'homeUser']);
